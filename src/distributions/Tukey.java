8f17ffbee1595a1c8967c3e117d09b39e81a3145
CheJinsen <983877388@qq.com>
2020-11-07 12:24:07 +0800

    update Studentized Range Distribution

--- src/distributions/Tukey.java
+++ src/distributions/Tukey.java
@@ -3,10 +3,9 @@ package distributions;
 import distributions.detail.Dpq;
 import special_functions.LogGamma;
 
-import java.math.BigDecimal;
-
 public class Tukey  // Studentized Range Distribution
 {
+    // Note: cdf() maybe have precision problem
     public static double cdf(double q, double nRanges, double nMeans, double df)
     {
         return cdf(q, nRanges, nMeans, df, true, false);
@@ -133,6 +132,84 @@ public class Tukey  // Studentized Range Distribution
         return Dpq.DTVal(ans, lower_tail, log_p);
     }
 
+    public static double quantile(double p, double nRanges, double nMeans, double df)
+    {
+        return quantile(p, nRanges, nMeans, df, true, false);
+    }
+
+    public static double quantile(double p, double nRanges, double nMeans, double df, boolean lower_tail)
+    {
+        return quantile(p, nRanges, nMeans, df, lower_tail, false);
+    }
+
+    public static double quantile(double p, double nRanges, double nMeans,
+                                  double df, boolean lower_tail, boolean log_p)
+    {
+        final double eps = 0.0001;
+        final int maxIterator = 50;
+
+        if (Double.isNaN(p) || Double.isNaN(nRanges) || Double.isNaN(nMeans) || Double.isNaN(df)) {
+            System.out.println("Argument out of domain in Tukey.quantile()");
+            return p + nRanges + nMeans + df;
+        }
+        if (df < 2 || nRanges < 1 || nMeans < 2) {
+            return Dpq.nanWarn();
+        }
+
+        double l = 0.0;
+        double r = Double.POSITIVE_INFINITY;
+        if (log_p) {
+            if (p > 0.0) {
+                return Dpq.nanWarn();
+            }
+            if (p == 0.0) {
+                return lower_tail ? r : l;
+            }
+            if (p == Double.NEGATIVE_INFINITY) {
+                return lower_tail ? l : r;
+            }
+        } else {
+            if (p < 0.0 || p > 1.0) {
+                return Dpq.nanWarn();
+            }
+            if (p == 0.0) {
+                return lower_tail ? l : r;
+            }
+            if (p == 1.0) {
+                return lower_tail ? r : l;
+            }
+        }
+
+        p = Dpq.DTqIv(p, lower_tail, log_p);
+        double x0 = quantileInv(p, nMeans, df);
+        double val_x0 = cdf(x0, nRanges, nMeans, df, true, false) - p;
+        double x1;
+        if (val_x0 > 0.0) {
+            x1 = Math.max(0.0, x0 - 1.0);
+        } else {
+            x1 = x0 + 1.0;
+        }
+
+        double val_x1 = cdf(x1, nRanges, nMeans, df, true, false) - p;
+        double ans = 0.0;
+        for(int it = 1 ; it < maxIterator ; it++) {
+            ans = x1 - ((val_x1 * (x1 - x0)) / (val_x1 - val_x0));
+            val_x0 = val_x1;
+            x0 = x1;
+            if (ans < 0.0) {
+                ans = 0.0;
+            }
+
+            val_x1 = cdf(ans, nRanges, nMeans, df, true, false) - p;
+            x1 = ans;
+            if (Math.abs(x1 - x0) < eps)
+                return ans;
+        }
+
+        System.out.println("Convergence failed in Tukey.quantile()");
+        return ans;
+    }
+
     private static double wProb(double w, double rr, double cc)
     {
         final int nLeg = 12;
@@ -233,10 +310,44 @@ public class Tukey  // Studentized Range Distribution
         return Math.min(pr_w, 1.0);
     }
 
+    private static double quantileInv(double p, double c, double v)
+    {
+        final double p0 = 0.322232421088;
+        final double q0 = 0.993484626060e-01;
+        final double p1 = -1.0;
+        final double q1 = 0.588581570495;
+        final double p2 = -0.342242088547;
+        final double q2 = 0.531103462366;
+        final double p3 = -0.204231210125;
+        final double q3 = 0.103537752850;
+        final double p4 = -0.453642210148e-04;
+        final double q4 = 0.38560700634e-02;
+        final double c1 = 0.8832;
+        final double c2 = 0.2368;
+        final double c3 = 1.214;
+        final double c4 = 1.208;
+        final double c5 = 1.4142;
+        final double vMax = 120.0;
+
+        double ps = 0.5 - 0.5 * p;
+        double yi = Math.sqrt(Math.log(1.0 / (ps * ps)));
+        double t = yi + (((( yi * p4 + p3) * yi + p2) * yi + p1) * yi + p0)
+                / (((( yi * q4 + q3) * yi + q2) * yi + q1) * yi + q0);
+        if (v < vMax) {
+            t += (t * t * t + t) / v / 4.0;
+        }
+        double q = c1 - c2 * t;
+        if (v < vMax) {
+            q += -c3 / v + c4 * t / v;
+        }
+        return t * (q * Math.log(c - 1.0) + c5);
+    }
+
     public static void main(String[] args)
     {
         // Maybe have precision's problem after 15th digit point
         System.out.println(cdf(7, 10, 60, 5));
         System.out.println(cdf(70, 100, 60, 5));
+        System.out.println(quantile(0.7, 10, 60, 5));
     }
 }
